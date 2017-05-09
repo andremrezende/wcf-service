7a4b00804f13444fa9a9c7440d0d4d341d751acb
Andre Rezende <meirezende@hotmail.com>
2017-05-09 19:29:08 -0300

    Attributes added in PartData.

--- WcfService/PartData.cs
+++ WcfService/PartData.cs
@@ -1,6 +1,14 @@
-﻿namespace WcfService
+﻿using System;
+
+namespace WcfService
 {
     public class PartData
     {
+        public int Id { get; set; }
+        public int Area { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public bool Status { get; set; }
+        public TimeSpan ProcTime { get; set; }
     }
 }
